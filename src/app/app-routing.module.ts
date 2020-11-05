5e7add04b0e377890dd874420c21be2bdf8f7669
Andrzej Rotarski <rotarskia@gmail.com>
2020-11-05 18:22:21 +0100

    klub commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { WelcomComponent } from './main/welcom/welcom.component';
+import { EditProfilComponent } from './parts/klub/edit-profil/edit-profil.component';
+import { ProfilComponent } from './parts/klub/profil/profil.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'', component: WelcomComponent},
+  { path: 'klub', component:ProfilComponent,
+    children:[
+      { path: 'edit', component: EditProfilComponent}
+    ]},
+  {path:'**', component: WelcomComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WelcomComponent } from './main/welcom/welcom.component';
+import { HeaderComponent } from './main/header/header.component';
+import { FooterComponent } from './main/footer/footer.component';
+import { ProfilComponent } from './parts/klub/profil/profil.component';
+import { EditProfilComponent } from './parts/klub/edit-profil/edit-profil.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomComponent,
+    HeaderComponent,
+    FooterComponent,
+    ProfilComponent,
+    EditProfilComponent
   ],
   imports: [
     BrowserModule,
